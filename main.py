6a6cc9b8601076a4748a79de66d34b3f764f810f
vdubchak <@janexxx1337>
2023-12-22 16:43:23 +0300

    fix comments

--- main.py
+++ main.py
@@ -36,43 +36,43 @@ def apply_clahe(image):
 
 def pasp_read(photo):
     image = photo
+
+    # Преобразование в градации серого для упрощения обработки
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     (H, W) = gray.shape
 
-    nameAreaTopLeftX = int(W * 0.05)
-    nameAreaTopLeftY = int(H * 0.15)
-    nameAreaWidth = int(W * 0.9)
-    nameAreaHeight = int(H * 0.3)
-    nameArea = image[nameAreaTopLeftY:nameAreaTopLeftY + nameAreaHeight,
-               nameAreaTopLeftX:nameAreaTopLeftX + nameAreaWidth]
-    cv2.rectangle(image, (nameAreaTopLeftX, nameAreaTopLeftY),
-                  (nameAreaTopLeftX + nameAreaWidth, nameAreaTopLeftY + nameAreaHeight),
-                  (0, 255, 0), 2)
-
-    # Определение ядер для морфологических операций
+    # Создание структурных элементов для морфологических операций
     rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25, 7))
     sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 21))
 
-    # Предобработка изображения
+    # Гауссово размытие для сглаживания шума
     gray = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    # Выделение черных участков на светлом фоне
     blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKernel)
+
+    # Вычисление градиента для выделения границ
     grad = cv2.Sobel(blackhat, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
     grad = np.absolute(grad)
     (minVal, maxVal) = (np.min(grad), np.max(grad))
     grad = (grad - minVal) / (maxVal - minVal)
     grad = (grad * 255).astype("uint8")
+
+    # Закрытие для соединения смежных элементов
     grad = cv2.morphologyEx(grad, cv2.MORPH_CLOSE, rectKernel)
+
+    # Пороговое преобразование для бинаризации изображения
     thresh = cv2.threshold(grad, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
     thresh = cv2.erode(thresh, None, iterations=2)
 
-    # Поиск контуров
+    # Поиск контуров на изображении
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     cnts = sort_contours(cnts, method="bottom-to-top")[0]
-    mrzBox = None
 
-    # Поиск MRZ
+    # Поиск MRZ на основе размеров контура
+    mrzBox = None
     for c in cnts:
         (x, y, w, h) = cv2.boundingRect(c)
         percentWidth = w / float(W)
@@ -80,69 +80,55 @@ def pasp_read(photo):
         if percentWidth > 0.29 and percentHeight > 0.005:
             mrzBox = (x, y, w, h)
             break
+
+    # Обработка ситуации, когда MRZ не найден
     if mrzBox is None:
         print("[INFO] MRZ could not be found")
         sys.exit(0)
 
-    # Выделение MRZ
+    # Выделение и коррекция области MRZ
     (x, y, w, h) = mrzBox
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     pX = int((x + w) * 0.03)
     pY = int((y + h) * 0.083)
     (x, y) = (x - pX, y - pY)
     (w, h) = (w + (pX * 2), h + (pY * 2))
     mrz = image[y:y + h, x:x + w]
 
-    # Распознавание текста MRZ
-    config = "--oem 3 --psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><"
+    # Использование OCR для извлечения текста из MRZ
+    config = (" --oem 3 --psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><")
     mrzText = pytesseract.image_to_string(mrz, lang='eng', config=config)
+
+    # Обработка и структурирование извлеченных данных
     mrzText = mrzText.replace(" ", "")
     mrzText = mrzText.split()
-
-    # Проверка и коррекция данных MRZ
     if mrzText[0][0:1] != 'P':
         del mrzText[0]
-    el1 = mrzText[0].replace('1', 'I')
-    el2 = mrzText[1].replace('O', '0')
-
-    # Разделение и обработка данных MRZ
-    el1 = re.split("<<|<|\n", el1[5:])
+    el1 = mrzText[0]
+    el2 = mrzText[1]
+    el1 = el1.replace('1', 'I')
+    el2 = el2.replace('O', '0')
+    el1 = el1[5:]
+    el1 = re.split("<<|<|\n", el1)
     el2 = re.split("RUS|<", el2)
     el1 = list(filter(None, el1))
-    el1 = [list(x) for x in el1][0:3]
+    el1 = list(map(list, el1))
+    el1 = el1[0:3]
     el2 = list(filter(None, el2))
-
-    # Замена символов английские на русские
     for i in el1:
         for c, j in enumerate(i):
             ind = eng.index(str(j))
             i[c] = rus[ind]
-
-    # Сбор данных
     surname = ''.join(el1[0])
     name = ''.join(el1[1])
-    otch = ''.join(el1[2]) if len(el1) > 2 else ''
+    otch = ''.join(el1[2])
     seria = el2[0][0:3] + el2[2][0:1]
     nomer = el2[0][3:9]
     data = el2[1][0:6]
-    data = '19' + data if int(data[0:1]) > 2 else '20' + data
+    if int(data[0:1]) > 2:
+        data = '19' + data
+    else:
+        data = '20' + data
     data = data[6:8] + '.' + data[4:6] + '.' + data[0:4]
-
-    # Распознавание текста в верхней части документа
-    nameText = pytesseract.image_to_string(nameArea, lang='rus', config='--psm 6')
-    nameText = re.sub(r'\s+', ' ', nameText).strip()
-
-    # Вывод сравнения
-    print("Сравнение имени, фамилии и отчества:")
-    print(f"MRZ: Фамилия - {surname}, Имя - {name}, Отчество - {otch}")
-    print(f"Верхняя часть: {nameText}")
-
-    # Определение результата сравнения
-    nameParts = nameText.split()
-    comparisonResult = (surname == nameParts[0] and name == nameParts[1] and (
-        otch == nameParts[2] if len(nameParts) > 2 else ''))
-    print(f"Результат сравнения: {'совпадает' if comparisonResult else 'не совпадает'}")
-
     global pasdata
     pasdata = {'Surname': surname, 'Name': name, 'Mid': otch, 'Date': data, 'Series': seria, 'Number': nomer}
     return pasdata
